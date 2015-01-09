5d80f051cd4143e92cb2d5725d0ea98437a5383c
Mark Perkins <markeperk@gmail.com>
2015-01-09 15:38:57 -0700

    save

--- arraysPractice.js
+++ arraysPractice.js
@@ -3,66 +3,77 @@
 
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item the given array.
-
   //Code Here
 
+var first = function(arr) {
+  var arr = [0];
+  return arr;
+}
 
 //Next problem
-
-
 
 var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item the given array.
-
-
   //Code Here
 
+var last = function(arr) {
+  var arr = arr[arr.length-1]
+  return arr;
+}
 
 //Next Problem
-
 
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
-
   //Code Here
 
+var looper = function(family) {
+  for (var i = 0; i < family.length; i++) {
+    alert(family[i]);
+  }
+};
 
 //Next problem
-
-
 
 var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
-
   //Code Here
 
+var reversedLooper = function(arr) {
+  for (var i = arr.length-1; i >= 0; i--) {
+    alert(arr[i]);
+  }
+};
 
 //Next Problem
 
-
 var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
-
   //Code Here
 
+var evenFinder = function(arr) {
+  for (var i = 0; i < nums.length; i++) {
+    if(arr[i] % 2 === 1) {
+     arr.splice(i, 1);
+     i--;
+    }
+  }
+};
 
 //Next problem
-
 
 var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 var evens = [];
 var odds = []
 //Write a function called divider that is given three arguments, nums, evens, and odds.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
-
-
-
   //Code Here
+
+var divider = 
+
 
 
 //Next Problem
-
-
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
